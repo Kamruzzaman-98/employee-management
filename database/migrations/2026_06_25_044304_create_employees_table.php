6852a85bebef5996d3686ec97b59ef41171a9666
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-06-27 16:31:46 +0600

    add user id in employee table

--- database/migrations/2026_06_25_044304_create_employees_table.php
+++ database/migrations/2026_06_25_044304_create_employees_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->nullable();
