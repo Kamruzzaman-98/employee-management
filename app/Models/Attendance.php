6370ec887946baf5e8cb314705bd8f624d9266ea
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-06-30 14:35:43 +0600

    add attendance model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,15 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Attendance extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'employee_id',
-        'date',
+        'attendance_date',
         'check_in',
         'check_out',
+        'working_minutes',
+        'late_minutes',
+        'overtime_minutes',
         'status',
+        'remarks',
+    ];
+
+    protected $casts = [
+        'attendance_date' => 'date',
+        'check_in' => 'datetime',
+        'check_out' => 'datetime',
     ];
 
     public function employee()
