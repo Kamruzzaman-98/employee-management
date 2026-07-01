3b942bcc62b6815adff75273c866d8232ab91e30
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-07-01 14:51:13 +0600

    Leave model update

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Leave extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'employee_id',
         'leave_type_id',
@@ -17,4 +20,19 @@ class Leave extends Model
         'approved_by',
         'approved_at',
     ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function leaveType()
+    {
+        return $this->belongsTo(LeaveType::class);
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
 }
