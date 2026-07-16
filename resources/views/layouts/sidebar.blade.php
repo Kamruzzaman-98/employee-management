68034e49846408b8d2ba768a4a8c4d3c5fb663b6
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-07-16 16:22:04 +0600

    Logout problem fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -80,9 +80,14 @@
         </li> --}}
 
         <li>
-            <a href="{{ route('logout') }}">
+            <a href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 🚪 Logout
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
+                @csrf
+            </form>
         </li>
 
     </ul>
